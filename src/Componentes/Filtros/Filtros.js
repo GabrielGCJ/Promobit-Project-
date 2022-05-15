1671e9a80b8db30afb82f949ba794d466de66647
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-05-15 16:32:37 -0300

    Update componentes de filtragem

--- src/Componentes/Filtros/Filtros.js
+++ src/Componentes/Filtros/Filtros.js
@@ -3,14 +3,12 @@ import { ContainerFilter, ContainerApresentaao, ContainerTexto} from "./styled"
 const Filtros = () => {
 
     return(
-        <ContainerFilter>
-            
+        <ContainerFilter>            
             <ContainerApresentaao>
                 <ContainerTexto>
                     <h1> Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
                 </ContainerTexto>  
-            </ContainerApresentaao>
-            
+            </ContainerApresentaao>            
             <p> Filtre por: </p>
         </ContainerFilter>
     )
--- src/Componentes/Filtros/styled.js
+++ src/Componentes/Filtros/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export const ContainerFilter = styled.div`
 
     @media screen and ( min-device-width : 10px ) and (max-device-width : 249px) {
+        
         background-color: rgb(45,12,93);
         height:300px ;
         
@@ -12,6 +13,7 @@ export const ContainerFilter = styled.div`
     }
 
     @media screen and ( min-device-width : 250px ) and (max-device-width : 480px) {
+        
         background-color: rgb(45,12,93);
         height:300px ;
         
@@ -21,6 +23,7 @@ export const ContainerFilter = styled.div`
     }
 
     @media screen and (min-device-width : 481px) and (max-device-width : 2000px) {    
+        
         background-color: rgb(45,12,93);
         height:300px ;
 
@@ -34,28 +37,34 @@ export const ContainerFilter = styled.div`
 export const ContainerApresentaao = styled.div`
 
     @media screen and ( min-device-width : 10px ) and (max-device-width : 249px) {
-        background-color: green;
+        
         h1{
            padding-top:20px ;
+           padding-bottom: 20px;
+           padding-left: 5px;
+           position: relative;
         }
     }
 
     @media screen and ( min-device-width : 250px ) and (max-device-width : 480px) {        
-        background-color: green;
+        
         h1{
             padding-top:20px ;
+            padding-bottom: 20px;
+            padding-left: 5px;
+            position: relative;
         }
     }
 
     @media screen and (min-device-width : 481px) and (max-device-width : 2000px) { 
-        background-color: green;
-        padding-top: 30px ;
+        
         display:flex ;
         align-items: center;
         justify-content: center;
         
         h1{
             padding-top:20px ;
+            padding-bottom: 20px;
         }
     }
 `
@@ -70,8 +79,7 @@ export const ContainerTexto = styled.div`
     @media screen and ( min-device-width : 250px ) and (max-device-width : 480px) {
         width: 90%;
         align-items:center ;
-    }
-    
+    }    
 
     @media screen and (min-device-width : 481px) and (max-device-width : 2000px) { 
         width: 60%;
