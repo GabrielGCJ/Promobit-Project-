6c4e5233bbcf403d0659491c3a758be52455e305
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-05-16 21:56:17 -0300

    Ajustes Css

--- src/Componentes/Filtros/styled.js
+++ src/Componentes/Filtros/styled.js
@@ -5,31 +5,34 @@ export const ContainerFilter = styled.div`
     @media screen and ( min-device-width : 10px ) and (max-device-width : 249px) {
         
         background-color: rgb(45,12,93);
-        height:300px ;
+        height:700px ;
         
         p{
-            padding-top: 20px;
+            padding-top: 10px;
+            color: white;
         }
     }
 
     @media screen and ( min-device-width : 250px ) and (max-device-width : 480px) {
         
         background-color: rgb(45,12,93);
-        height:300px ;
+        height:400px ;
         
         p{
-            padding-top: 20px;
+            padding-top: 10px;
+            color: white;
         }
     }
 
     @media screen and (min-device-width : 481px) and (max-device-width : 2000px) {    
         
         background-color: rgb(45,12,93);
-        height:300px ;
+        height: 350px ;
 
         p{
-            padding-top: 20px;
+            padding-top: 10px;
             text-align:center ;
+            color: white;
         }        
     }
 `
@@ -43,6 +46,7 @@ export const ContainerApresentaao = styled.div`
            padding-bottom: 20px;
            padding-left: 5px;
            position: relative;
+           color: white;
         }
     }
 
@@ -53,6 +57,7 @@ export const ContainerApresentaao = styled.div`
             padding-bottom: 20px;
             padding-left: 5px;
             position: relative;
+            color: white;
         }
     }
 
@@ -65,6 +70,7 @@ export const ContainerApresentaao = styled.div`
         h1{
             padding-top:20px ;
             padding-bottom: 20px;
+            color: white;
         }
     }
 `
